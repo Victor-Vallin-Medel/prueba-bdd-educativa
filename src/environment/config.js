b3805f79a380a586386ee4e5f5e52d47e82e3e94
Daniel Molina <danmnvx@gmail.com>
2020-06-13 20:33:09 -0500

    Variables de entorno

---
+++ src/environment/config.js
@@ -0,0 +1,8 @@
+// App port
+process.env.PORT = process.env.PORT || 3000;
+
+// Scret key for JWT
+process.env.PRIVATE_KEY = process.env.PRIVATE_KEY || '1029pr0y3ct0Fin4l_b4s3_d3_d4t05_uaa5647';
+
+// JWT expiration
+process.env.EXPIRATION = process.env.EXPIRATION || '48h';
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// Config
+require('./environment/config');
+
 const database = require('./database');
 const express = require('express');
 const morgan = require('morgan');
@@ -7,7 +10,7 @@ const app = express();
 database.initMongo();
 
 //Settings
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT);
 
 //Middlewares 
 app.use(morgan('dev'));
@@ -15,6 +18,7 @@ app.use(express.json());
 app.use(cors());
 
 //Routes
+app.use('/auth', require('./routes/auth.routes'));
 app.use('/api/subCategoria', require('./routes/subcategoria.routes'));
 app.use('/api/Categoria', require('./routes/categoria.routes'));
 app.use('/api/Curso', require('./routes/curso.routes'));
@@ -23,6 +27,6 @@ app.use('/api/UsuarioContenido', require('./routes/usuario_contenido.routes'));
 app.use('/api/Conversacion', require('./routes/conversacion.routes'));
 
 //server is listening
-app.listen(app.get('port'), () => {
+app.listen(process.env.PORT, () => {
     console.log('Server on port', app.get('port'));
 });
