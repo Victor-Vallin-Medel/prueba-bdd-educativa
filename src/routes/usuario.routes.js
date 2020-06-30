a0b8bf3aaa5121350d41f12bfe7cd33f248dea5a
Daniel Molina <danmnvx@gmail.com>
2020-06-30 00:34:36 -0500

    Actualización de contraseña

--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const router =  express.Router();
 
 const Usuario = require('../models/usuario');
@@ -15,13 +16,26 @@ router.get('/', async (req, res) =>{
     res.json(usuarios);
 });
 
+// Update passwd
+router.post('/passwd/:id', async (req, res) => {
+    const passwd = bcrypt.hashSync(req.body.passwd, 7);
+    await Usuario.findByIdAndUpdate(req.params.id, { passwd: passwd }, (erro, user) => {
+        if (erro) res.status(500).json({ ok: false, err: erro });
+
+        if (!user) res.status(400).json({ ok: false, err: "Usuario no encontrado" });
+
+        res.json({ ok: true });
+    })
+});
+
+// Update info user
 router.patch('/:id', async (req, res) => {
     await Usuario.findByIdAndUpdate(req.params.id, req.body, { new: true }, (erro, user) => {
         if (erro) res.status(500).json({ ok: false, err: erro });
 
         if (!user) res.status(400).json({ ok: false, err: "Usuario no encontrado" });
 
-        res.json({ ok: true, usuario: user })
+        res.json({ ok: true, usuario: user });
     });
 });
 
