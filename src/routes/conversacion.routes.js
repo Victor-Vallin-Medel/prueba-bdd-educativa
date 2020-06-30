a87525ad20687f7412f852d27a5b9166cf46f574
Daniel Molina <danmnvx@gmail.com>
2020-06-30 01:24:48 -0500

    Creación de conversación

--- src/routes/conversacion.routes.js
+++ src/routes/conversacion.routes.js
@@ -3,17 +3,34 @@ const router =  express.Router();
 
 const Conversacion = require('../models/conversacion');
 
-router.post('/',  async (req, res) =>{
-    await Conversacion.insertMany([req.body]);
-    res.json({
-        status: "200"
-    })
+router.post('/init/:id',  async (req, res) =>{
+    const body = req.body;
+    const transmitter = req.params.id;
+
+    let message = new Conversacion({
+        miembros: [transmitter, body.receiver],
+        mensajes: [{
+            IdAutor: transmitter,
+            cuerpo: body.msg,
+            fechEnviado: new Date()
+        }]
+    });
+    
+    await message.save((erro, chat) => {
+        // Error
+        if (erro) return res.status(500).json({ ok: false, err: erro });
+        
+        // Success
+        res.json({ ok: true, message: chat.mensajes.pop() });
+    });
 });
 
 router.get('/', async (req, res) =>{
     const conversaciones = await Conversacion.find();
     res.json(conversaciones);
 });
+
+// router.get();
 
 router.put('/:id', async (req, res) => {
     await Conversacion.findByIdAndUpdate(req.params.id, req.body);
