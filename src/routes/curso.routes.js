c6adbd4af9b126457d9ed7ecd18773971734cd46
Victor-Vallin-Medel <57019259+Victor-Vallin-Medel@users.noreply.github.com>
2020-07-01 09:07:43 -0500

    Adecuacion de cursos

--- src/routes/curso.routes.js
+++ src/routes/curso.routes.js
@@ -3,8 +3,10 @@ const router =  express.Router();
 
 const Curso = require('../models/curso');
 
+const Usuario = require('../models/usuario');
+
 router.post('/',  async (req, res) =>{
-    await Curso.insertMany([req.body]);
+    await Curso.insertMany([req.body.curso]);
     res.json({
         status: "200"
     })
@@ -15,18 +17,24 @@ router.get('/', async (req, res) =>{
     res.json(cursos);
 });
 
-router.put('/:id', async (req, res) => {
-    await Curso.findByIdAndUpdate(req.params.id, req.body);
-    res.json({
-        status: "200"
-    });
-});
-
 router.delete('/:id', async (req, res) => {
     await Curso.findByIdAndDelete(req.params.id);
     res.json({
         status: "200"
     });
+});
+
+//Inscripcion a curso
+router.put('/inscripcionCurso', async (req, res) => {
+    let body = req.body;
+    const user = body.user;
+    const newCourses = user.cursos.concat({ idCurso: body.idCurso, finalizado: false });
+    await Usuario.findByIdAndUpdate(user._id, {$set:{cursos: newCourses}}, {new: true}, (erro, doc) => {
+        if(erro){
+            res.status(500).json({ ok: false, err: erro});
+        } else {
+            res.status(200).json({ ok: true })};
+        });
 });
 
 module.exports = router;
